fdf63271dcf99e15431afe87a382d56d71752bd8
ArafatAkashAkku <arafatakash5@gmail.com>
2023-06-24 13:18:25 +0600

    Sections Added

---
+++ js/index.js
@@ -0,0 +1,27 @@
+// navigation bar on scroll effect and scroll progress bar
+const navigationBar = document.getElementById('navigation-bar');
+const container = document.querySelector("body");
+const highlight = document.getElementById("bar-highlight");
+
+// windows scroll function 
+window.onscroll = () => {
+    // scroll effect navbar
+    if (scrollY > 80) {
+        navigationBar.style.backgroundColor = "yellow";
+    }
+    else {
+        navigationBar.style.backgroundColor = "transparent";
+    }
+    // scroll progress bar
+    let cheight = container.offsetHeight - window.innerHeight;
+    let cpos = container.getBoundingClientRect();
+    let diff = cheight + cpos.top;
+    let progress = diff / cheight * 100;
+    let csswidth = Math.floor(100 - progress);
+    highlight.style.width = csswidth + "%";
+}
+
+// right click disable 
+document.oncontextmenu=(element)=>{
+    element.preventDefault();
+}
